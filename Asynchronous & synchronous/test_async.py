e534a4a5fa0e73b9286647256c4d508599470e12
‘user <pphyo3469@gmail.com>
2022-11-12 11:16:45 +0630

    first commit

---
+++ Asynchronous & synchronous/test_async.py
@@ -0,0 +1,15 @@
+import asyncio
+
+async def main():
+    task = asyncio.create_task(otherFunction())
+    print("A")
+    await asyncio.sleep(1)
+    print("B")
+    await task
+
+async def otherFunction():
+    print("1")
+    await asyncio.sleep(2)
+    print("2")
+
+asyncio.run(main())
